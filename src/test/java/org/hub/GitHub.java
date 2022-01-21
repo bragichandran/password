4f0609f21a145d3c3e7c78b20c30ee9114064dd2
bragichandran <bragadeeshsivaramachandran@gmail.com>
2022-01-21 14:03:57 +0530

    Folder Structure

---
+++ src/test/java/org/hub/GitHub.java
@@ -0,0 +1,11 @@
+package org.hub;
+
+public class GitHub {
+	public void login() {
+		System.out.println("login");
+	}
+ public static void main(String[] args) {
+	GitHub g = new GitHub();
+	g.login();
+}
+}
